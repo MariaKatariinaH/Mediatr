bf31cb5fc8d1942b97277a56e6ab277348e0b07d
MariaKatariinaH <emaha036@edu.xamk.fi>
2025-04-18 12:14:03 +0300

    tehty entiteetti Team

---
+++ StudentEfCoreDemo.Domain/Entities/Team.cs
@@ -0,0 +1,70 @@
+using System;
+
+namespace StudentEfCoreDemo.Domain.Entities
+{
+    public class Team
+    {
+        private string _name;
+        private string _sportType;
+        private string _homeStadium;
+        private int _maxRosterSize;
+
+        public int Id { get; set; }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Name cannot be empty or whitespace.", nameof(Name));
+                _name = value;
+            }
+        }
+
+        public string SportType
+        {
+            get => _sportType;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Sport type cannot be empty or whitespace.", nameof(SportType));
+                _sportType = value;
+            }
+        }
+
+        public DateTime FoundedDate 
+        { get => _foundedDate;
+            set
+            {
+                if (value > DateTime.Now)
+                    throw new ArgumentException("Founding date cannot be in the future,", nameof(FoundedDate));
+                _foundedDate = value;
+            }
+        }
+
+        public string HomeStadium
+        {
+            get => _homeStadium;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Home stadium cannot be empty or whitespace.", nameof(HomeStadium));
+                _homeStadium = value;
+            }
+        }
+
+
+
+        public int MaxRosterSize
+        {
+            get => _maxRosterSize;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("MaxRosterSize cannot be negative.", nameof(MaxRosterSize));
+                _maxRosterSize = value;
+            }
+        }
+    }
+} 
